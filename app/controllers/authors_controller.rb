74172ef5012a162481507fc4ec3c2bf3f8865cd6
Pallav Sharma <pallavps17@gmail.com>
2022-08-19 10:31:47 +0530

    Backend Connectivity
    Backend Connectivity Commit

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,30 +1,30 @@
 class AuthorsController < ApplicationController
 
     def addauthor
-        a = params[:author_id]
-        b = params[:author_name]
-        c = params[:author_email]
-        d = params[:author_password]
-        e = params[:author_registereddate]
-        f = parmas[:author_lastlogindate]
-        g = params[:author_bio]
+        # a = params[:author_id]
+        b = params[:Name]
+        c = params[:Email]
+        d = params[:Password]
+        # e = params[:author_registereddate]
+        # f = parmas[:author_lastlogindate]
+        g = params[:Bio]
 
-        author = Author.new(author_id:a,author_name:b,author_email:c,author_password:d,author_registereddate:e,author_lastlogindate:f,author_bio:g)
+        author = Author.new(Name:b,Email:c,Password:d,Bio:g)
         author.save
         render :json => author
     end
     
     def editauthor
         author = Author.find(params[:author_id])
-        a = params[:author_id]
-        b = params[:author_name]
-        c = params[:author_email]
-        d = params[:author_password]
-        e = params[:author_registereddate]
-        f = parmas[:author_lastlogindate]
-        g = params[:author_bio]
+        a = params[:id]
+        b = params[:Name]
+        c = params[:Email]
+        d = params[:Password]
+        # e = params[:author_registereddate]
+        # f = parmas[:author_lastlogindate]
+        g = params[:Bio]
 
-        author.update(author_id:a,author_name:b,author_email:c,author_password:d,author_registereddate:e,author_lastlogindate:f,author_bio:g)
+        author.update(id:a,Name:b,Email:c,Password:d,Bio:g)
         author.save
     end
 
